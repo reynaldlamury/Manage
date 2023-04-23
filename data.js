72a59205426481d00264b3ae8a01c2b7be4e5759
reynaldlamury <51454984+reynaldlamury@users.noreply.github.com>
2023-04-23 18:24:17 +0700

    create basic layout for carousel

---
+++ data.js
@@ -0,0 +1,32 @@
+import ali from "./src/images/avatar-ali.png";
+import anisha from "./src/images/avatar-anisha.png";
+import shanai from "./src/images/avatar-shanai.png";
+import richard from "./src/images/avatar-richard.png";
+
+const cards = [
+  {
+    name: "Anisha Li",
+    img: ali,
+    desc: "Manage has supercharged our team's workflow. The ability to maintain visibitily on larger milestone at all times keeps everyone motivated",
+  },
+
+  {
+    name: "Ali Bravo",
+    img: anisha,
+    desc: "We have been able to cansel so many other subscription since using Manage. There is no more cross-channel confusion and everyone is much more focused",
+  },
+
+  {
+    name: "Richard Watts",
+    img: richard,
+    desc: "Manage allows us to provide structure and process. It keeps us organized and focused. I can't stop recommending them to everyone I talk to!",
+  },
+
+  {
+    name: "Shanai Gough",
+    img: shanai,
+    desc: "Their software allows us to track, manage and collaborate on our projects from anywhere. It keeps the whole team in sync without being instrusive.",
+  },
+];
+
+export default cards;
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,5 +1,21 @@
+import cards from "../../data";
+
 const Carousel = () => {
-  return <h1>Carousel</h1>;
+  return (
+    <>
+      <div className="wrapper">
+        <div className="card-container">
+          {cards.map(({ name, img, desc }, idx) => (
+            <div key={idx} className="card">
+              <img className="card__img" src={img} alt={`${name}`} />
+              <p className="card__name margin-top-1em text-center">{name}</p>
+              <p className="card__desc margin-top-1em text-center">{desc}</p>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default Carousel;
